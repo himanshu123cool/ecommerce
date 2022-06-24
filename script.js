f1ce36b511fe26082116cc9ba51c1c32ac0103a1
himanshucool123 <himanshu312cool@gmail.com>
2022-06-24 22:13:06 +0530

    ecommerce

---
+++ script.js
@@ -0,0 +1,33 @@
+// more-option-script 
+$('#more').mouseenter(function(){
+    $('.right-wrapper-container').show()
+})
+$('.right-wrapper-container').mouseenter(function(){
+    $('.right-wrapper-container').show()
+})
+
+$('.right-wrapper-container').mouseleave(function(){
+    $('.right-wrapper-container').hide()
+})
+
+
+// newArr-wishlist-script 
+$('.newArr-wishlist').click(function(){
+    $(this).toggleClass('active')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
